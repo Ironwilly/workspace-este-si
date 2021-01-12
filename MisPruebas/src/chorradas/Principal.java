01b1a28bfed3d7a1567e02f0956bdb11c88cb38f
Guillermo <claseswilly@gmail.com>
2021-01-13 00:34:55 +0100

    Casa 13 enero 2021

---
+++ MisPruebas/src/chorradas/Principal.java
@@ -0,0 +1,45 @@
+package chorradas;
+
+import java.util.Random;
+
+import utilidades.Leer;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int [][]primero;
+		
+		int tam1,tam2;
+		int desde = 0, hasta = 9;
+		int ale;
+		
+		Random r;
+		r=new Random(System.nanoTime());
+		
+		System.out.println("Tamaño de las colunnas: \n");
+		tam1=Leer.datoInt();
+		
+		System.out.println("Tamaño de las filas: \n");
+		tam2=Leer.datoInt();
+		primero = new int[tam1][tam2];
+		
+		for (int i=0;i < primero.length ; i++) {
+			for(int j=0; j <primero[i].length ; j++) {
+				
+			primero[i][j]=0;
+			System.out.print(primero[i][j]+ " ");
+			}
+			
+		System.out.println("\t");
+			
+		}
+		
+		
+		
+
+	}
+
+}
