f15b9c8479ad60ae69b0c18f1dfdea24bd262210
Guillermo Ferrari Ferrari <claseswilly@gmail.com>
2021-03-22 12:10:17 +0100

    Clases 22 marzo 2021

--- PruebaLocalDate/src/prueba01/Principal.java
+++ PruebaLocalDate/src/prueba01/Principal.java
@@ -3,6 +3,8 @@ package prueba01;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 
@@ -32,9 +34,53 @@ public class Principal {
 	LocalDate fecha = LocalDate.now();
 	DateTimeFormatter f4 = DateTimeFormatter.ofPattern("dd  MMM yyyy E ");
 	String este = fecha.format(f4);
-	LocalDate fecha2 = LocalDate.parse(este, f4);
+	LocalDate fech = LocalDate.parse(este, f4);
 	System.out.println(este);
+	
+	
+	LocalDate fecha1 = LocalDate.parse("2021-03-22");
+	LocalDate nuevaFecha = fecha1.plusDays(10);
+	System.out.println(fecha1);
+	System.out.println(nuevaFecha+"\n");
+	
+	LocalDate fecha2 = LocalDate.parse("2021-03-22");
+	LocalDate nuevaFecha2 = fecha2.plusMonths(3);
+	System.out.println(fecha2);
+	System.out.println(nuevaFecha2+"\n");
 		
+	
+	LocalDate fecha3 = LocalDate.parse("2021-03-22");
+	LocalDate nuevaFecha3 = fecha3.plusWeeks(15);
+	System.out.println(fecha3);
+	System.out.println(nuevaFecha3 +"\n");
+	
+	LocalDate fecha4 = LocalDate.parse("2021-03-22");
+	LocalDate nuevaFecha4 = fecha4.plusYears(10);
+	System.out.println(fecha4);
+	System.out.println(nuevaFecha4 +"\n");
+	
+	System.out.println(LocalDate.parse("2021-03-22").isBefore(LocalDate.parse("2022-03-22")));
+	System.out.println(LocalDate.parse("2021-03-22").isAfter(LocalDate.parse("2022-04-04")));
+	System.out.println(LocalDate.parse("2044-02-02").isLeapYear());
+	
+	
+	System.out.println(LocalDate.parse("2031-07-14").getDayOfWeek());
+	
+	
+	  LocalDate localDate = LocalDate.parse("1971-01-01");
+      System.out.println("LocalDate                : " + localDate);
+      System.out.println("LocalDate.toEpochDay()   : " + localDate.toEpochDay());
+      
+      
+      LocalDate copyLocalDate = LocalDate.ofEpochDay(localDate.toEpochDay());
+      System.out.println("Copied LocalDate         : " + copyLocalDate);
+      
+      LocalDate fecha5 = LocalDate.parse("2021-03-22");
+      ZonedDateTime zonedDateTime = fecha5.atStartOfDay(ZoneId.of("America/Los_Angeles"));
+      System.out.println("ZonedDateTime : " + zonedDateTime);
+	
+	
+	
 	}
 
 }
